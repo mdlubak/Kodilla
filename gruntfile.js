f7f2c6d05a0ec61cbf66c8ffb9b76ed6ea94c53c
Martyna <mdlubak@gmail.com>
2016-12-28 23:20:16 +0000

    Initial

---
+++ gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+  	sass: {
+  		options: {
+  			sourceMap: true
+  		},
+  		dist: {
+  			files: {
+  				'css/style.css': 'sass/style.sass'
+  			}
+  		}
+  	},
+
+  	imagemin: {
+  		dynamic: {
+  			files: [{
+  				expand: true,
+  				cwd: 'images/',
+  				src: ['**/*.{png,jpg,gif}'],
+  				dest: 'images/compressed/'
+  			}]
+  		}
+  	},
+	
+	watch: {
+		scripts: {
+			files: ['sass/style.sass'],
+			tasks: ['sass'],
+			options: {
+				spawn: false,
+			},
+		} 
+	}
+	
+  });
+  
+  // Load the plugins tasks 
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  
+  grunt.registerTask('default', ['sass', 'imagemin', 'watch']);
+
+};
